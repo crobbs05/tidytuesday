1d6f3de6c20f64f95fc6a8d038bb40efc0d742a7
Chauncey Robbs <chauncey.robbs1@gmail.com>
2025-02-19 18:18:27 -0500

    Update to Code 02-19-2025

--- code/TidyTuesdays_Week_7_2025_FBI_Agency_Levels.R
+++ code/TidyTuesdays_Week_7_2025_FBI_Agency_Levels.R
@@ -5,6 +5,7 @@
 #by: Chauncey Robbs
 
 
+
 #add necessary packages
 library(tidyverse)
 library(ggthemes)
@@ -111,23 +112,24 @@ geom_waffle(n_rows = 7,flip = TRUE,make_proportional = FALSE,size = .05, width =
     scale_fill_manual(values =c("TRUE"="#E6A600","FALSE"="#972136"))+
     
     labs(title = "Participation varies by Maryland law enforcement agencies when reporting crime data to the FBI.")+
-    labs(subtitle = ("Counties report at 93% to the FBI's National Incident-Based Reporting System (NIBRS) compared to 82% by city agencies."))+
+    labs(subtitle = ("Counties report at <span style ='color:#E6A600;'>**93%**</span> to the FBI's National Incident-Based Reporting System (NIBRS) compared to <span style ='color:#E6A600;'>**82%**</span>  by city agencies."))+
     labs(caption =  "#TidyTuesday 2025 - Week 07 | Source: FBI | By: @ETTS12.BSKY.SOCIAL")+
     
+  
     
     theme(plot.background = element_rect(fill = "#292929", color = NA))+
     theme(strip.text.x = element_text(vjust = 0.5,margin = ))+
     theme(strip.text = element_text(color = "#DCDCDC",size = 28,family = "Mitr",face ="bold",
          margin = margin(t =25,b=-20)))+
-    theme(plot.title = element_text(color = "#DCDCDC", size =28, hjust = .5,
+    theme(plot.title = element_markdown(color = "#DCDCDC", size =28, hjust = .5,
          family = "Mitr", face = "bold",margin = margin(t=15)))+
-    theme(plot.subtitle = element_text(color = "#DCDCDC", size =22,  hjust = .5,
+    theme(plot.subtitle = element_markdown(color = "#DCDCDC", size =22,  hjust = .5,
          family = "Mitr"))+
     theme(plot.caption.position = "plot",plot.caption = element_text(color = "#DCDCDC",
          size = 12.5, family ="Mitr", hjust = -.55,margin = margin(b = 5)))+
     annotate(geom = "text",x = 5,y = .25,hjust =.75,color = "#DCDCDC", size =5,family ="Mitr",
              label = "1 Box = 1 Law Enforcement Agency")+
-    coord_equal(expand = TRUE,clip = "off",ylim = c(0,13))
+   coord_equal(expand = TRUE,clip = "off",ylim = c(0,13))
    
 
 
